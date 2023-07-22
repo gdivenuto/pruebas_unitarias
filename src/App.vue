b0768fc5116700be0bf144c9858fba9aad2b5f11
Gabriel Divenuto <37304699+gdivenuto@users.noreply.github.com>
2023-07-22 11:05:39 -0300

    Se sube el ejemplo de pruebas unitarias con Jest

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+    import Counter from './components/Counter.vue'
+    //import Indecision from './components/Indecision.vue'
+
+    export default {
+        name: 'App',
+        components: {
+            Counter
+        }
+    }
+</script>
+
+<template>
+    <Counter :start="100" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+//import './css/styles.css'
+
+createApp(App).mount('#app')
